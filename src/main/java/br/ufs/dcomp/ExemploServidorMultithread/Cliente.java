8f2c8601b8e3f8f987637da559ba4514ac13ed65
Tarcisio <tarcisio@dcomp.ufs.br>
2018-01-23 17:08:11 +0000

    esboço inicial

---
+++ src/main/java/br/ufs/dcomp/ExemploServidorMultithread/Cliente.java
@@ -0,0 +1,15 @@
+package br.ufs.dcomp.ExemploServidorMultithread;
+
+import java.net.*;
+
+/**
+ * 
+ *
+ */
+public class Cliente 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
---
+++ src/main/java/br/ufs/dcomp/ExemploServidorMultithread/Servidor.java
@@ -0,0 +1,15 @@
+package br.ufs.dcomp.ExemploServidorMultithread;
+
+import java.net.*;
+
+/**
+ * 
+ *
+ */
+public class Servidor 
+{
+    public static void main( String[] args )
+    {
+        
+    }
+}
